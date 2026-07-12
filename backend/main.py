4c357a38671c88eb3b23de73261d8e29d95b98b3
Shreyansh Dubey <dubeyshreyansh20@gmail.com>
2026-07-12 17:51:59 +0530

    Update CORS settings

--- backend/main.py
+++ backend/main.py
@@ -17,11 +17,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://ai-resume-analyzer-lov4de4sc-shreyansh-dubey.vercel.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
